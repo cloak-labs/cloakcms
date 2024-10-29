164115896b96eb2dcbc885a10398d3c574bcd017
Kaelan Smith <kaelancsmith@gmail.com>
2024-10-29 13:36:20 -0700

    add edge case to stripTrailingSlash to fix homepage revalidation bug

--- core/dist/utils/stripTrailingSlash.js
+++ core/dist/utils/stripTrailingSlash.js
@@ -1,3 +1,7 @@
 export function stripTrailingSlash(url) {
+    if (!url)
+        return "";
+    if (url === "/")
+        return url;
     return url.replace(/\/$/, "");
 }
--- core/src/utils/stripTrailingSlash.ts
+++ core/src/utils/stripTrailingSlash.ts
@@ -1,3 +1,5 @@
 export function stripTrailingSlash(url: string): string {
+  if (!url) return "";
+  if (url === "/") return url;
   return url.replace(/\/$/, "");
 }
